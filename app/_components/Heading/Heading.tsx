f110e74435f3fd2346d5675fa30c0367bae6fe72
vivshaw <hey@vivsha.ws>
2026-03-30 22:51:45 -0400

    feat: add Heading component extracted from Basalt

---
+++ app/_components/Heading/Heading.tsx
@@ -0,0 +1,50 @@
+import clsx from "clsx"
+import { type ComponentPropsWithRef, forwardRef, type Ref } from "react"
+
+import styles from "./Heading.module.css"
+
+type HeadingElement = "h1" | "h2" | "h3" | "h4" | "h5" | "h6"
+
+type HeadingOwnProps<T extends HeadingElement> = {
+  as?: T
+  level: "1" | "2" | "3" | "4" | "5" | "6"
+}
+
+export type HeadingProps<T extends HeadingElement = "h1"> = HeadingOwnProps<T> &
+  Omit<ComponentPropsWithRef<T>, keyof HeadingOwnProps<T>>
+
+const resolveDefaultElement = {
+  "1": "h1",
+  "2": "h2",
+  "3": "h3",
+  "4": "h4",
+  "5": "h5",
+  "6": "h6",
+} as const
+
+const levelClasses = {
+  "1": styles.level1,
+  "2": styles.level2,
+  "3": styles.level3,
+  "4": styles.level3,
+  "5": styles.level3,
+  "6": styles.level3,
+} as const
+
+export const Heading = forwardRef(
+  <T extends HeadingElement = "h1">(
+    { as, level, className, ...props }: HeadingProps<T>,
+    ref: Ref<HTMLHeadingElement>,
+  ) => {
+    const Component = as ?? resolveDefaultElement[level]
+    return (
+      <Component
+        ref={ref}
+        className={clsx(styles.heading, levelClasses[level], className)}
+        {...props}
+      />
+    )
+  },
+) as <T extends HeadingElement = "h1">(
+  props: HeadingProps<T>,
+) => React.ReactElement | null
